b80973dc7c509dd30e6c080ec22cf1dac7f43fe4
dharamveergit <dharamveersingh.em@gmail.com>
2024-06-27 18:45:17 +0530

    chore: Update astro.config.mjs with new integrations and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import astroExpressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 import remark from "remark-directive";
 import { customAsidePlugin } from "./src/lib/aside/customAsidePlugin";
-import astroExpressiveCode from "astro-expressive-code";
-import mdx from "@astrojs/mdx";
 
 import robotsTxt from "astro-robots-txt";
 
@@ -21,24 +21,34 @@ export default defineConfig({
     "/about": "/about/general-information/",
     "/about/pricing": "/about/pricing/compare",
     "/blog/a/acc-akash-accelerationism": "/blog/a-acc-akash-accelerationism/",
-    "/roadmap": "https://github.com/orgs/akash-network/projects/5/views/1?layout=roadmap"
+    "/roadmap":
+      "https://github.com/orgs/akash-network/projects/5/views/1?layout=roadmap",
   },
   markdown: {
     // shikiConfig: {
     //   theme: theme,
     // },
-    remarkPlugins: [remark, customAsidePlugin]
+    remarkPlugins: [remark, customAsidePlugin],
   },
-  integrations: [tailwind(), sitemap(), react(), astroExpressiveCode({
-    themes: ["light-plus", "dark-plus"],
-    useDarkModeMediaQuery: true,
-    themeCssSelector: theme => `[data-theme='${theme.name}']`,
-    styleOverrides: {
-      terminalTitlebarForeground: "var(--theme-header-bg)",
-      terminalTitlebarDotsForeground: "var(--three-dots-bg)",
-      terminalTitlebarBackground: "var(--theme-header-bg)",
-      terminalTitlebarDotsOpacity: "1"
-    }
-  }), mdx(), robotsTxt()],
-  site: "https://akash.network"
+  integrations: [
+    tailwind(),
+    sitemap({
+      lastmod: new Date("2024-06-27").toISOString(),
+    }),
+    react(),
+    astroExpressiveCode({
+      themes: ["light-plus", "dark-plus"],
+      useDarkModeMediaQuery: true,
+      themeCssSelector: (theme) => `[data-theme='${theme.name}']`,
+      styleOverrides: {
+        terminalTitlebarForeground: "var(--theme-header-bg)",
+        terminalTitlebarDotsForeground: "var(--three-dots-bg)",
+        terminalTitlebarBackground: "var(--theme-header-bg)",
+        terminalTitlebarDotsOpacity: "1",
+      },
+    }),
+    mdx(),
+    robotsTxt(),
+  ],
+  site: "https://akash.network",
 });
