6ed63b29265367184a9a875145e8d7ae18cb1e8d
dharamveergit <dharamveersingh.em@gmail.com>
2024-10-04 22:06:45 +0530

    update _illustration3.astro, _illustration4.astro and prose-classes.ts

--- src/lib/prose-classes.ts
+++ src/lib/prose-classes.ts
@@ -54,7 +54,7 @@ const className = clsx(
   "md:prose-td:px-4",
   "md:prose-td:text-sm",
   // Images
-  "prose-img:my-0 prose-img:mx-auto prose-img:max-w-[70rem] prose-img:w-full [&_svg]:max-w-[70rem]  [&_svg]:mx-auto ",
+  "prose-img:my-0 prose-img:mx-auto prose-img:max-w-[70rem] prose-img:w-full [&_.image]:max-w-[70rem]   [&_.image]:mx-auto ",
   // Horizontal rules
   "prose-hr:border-defaultBorder",
   "prose-hr:mb-0",
