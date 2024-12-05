a987168fd964b0db873c7558331ac08ee1e7127a
dharamveergit <dharamveersingh.em@gmail.com>
2024-12-05 13:29:35 +0530

    Refactor community and token page components for improved visuals and functionality
    - Updated the responsibilities section with new SVG icons to enhance the visual representation of validator roles.
    - Refactored the AktFeatures component to replace the ArrowUpRight icon with a custom SVG, improving user interaction and aesthetics.
    - Enhanced the How It Works section by updating the icon for better clarity and engagement.
    - Modified the Akash Insiders page to improve the Carousel component's loading behavior, ensuring a smoother user experience.
    - These changes aim to enhance the overall user experience and visual consistency across the application.

--- src/components/token-page/aktfeatures.tsx
+++ src/components/token-page/aktfeatures.tsx
@@ -1,5 +1,3 @@
-import { ArrowUpRight } from "lucide-react";
-
 const AktFeatures = ({
   aktFeaturesSection,
   url,
@@ -91,8 +89,22 @@ const AktFeatures = ({
           className={`h-auto w-full transition-all duration-300`}
         />
 
-        <div className="absolute bottom-[4.5%] left-[0.6%] right-[2.6%] top-[0.6%]  flex items-center justify-center rounded bg-black bg-opacity-40 opacity-0 transition-all duration-300 group-hover:opacity-100">
-          <ArrowUpRight className="h-12 w-12 text-white opacity-80 transition-transform group-hover:scale-110" />
+        <div className="absolute bottom-[4.5%] left-[0.6%] right-[2.4%] top-[0.5%]  flex items-center justify-center rounded-[1%] bg-black bg-opacity-80 opacity-0 transition-all duration-300 group-hover:opacity-100">
+          <svg
+            width="82"
+            height="82"
+            viewBox="0 0 82 82"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M27.3333 27.3333H54.6667M54.6667 27.3333V54.6667M54.6667 27.3333L27.3333 54.6667M17.0833 10.25H64.9167C68.6906 10.25 71.75 13.3094 71.75 17.0833V64.9167C71.75 68.6906 68.6906 71.75 64.9167 71.75H17.0833C13.3094 71.75 10.25 68.6906 10.25 64.9167V17.0833C10.25 13.3094 13.3094 10.25 17.0833 10.25Z"
+              stroke="white"
+              stroke-width="3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </div>
       </a>
     </div>
