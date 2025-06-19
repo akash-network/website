dba30a104d3f8d174dbc8ac8f2a5d204ee8bc9c5
Dharamveer singh <dharamveersingh.em@gmail.com>
2025-06-19 15:43:15 +0530

    feat: implement livestream functionality and new confirmation pages for acc

---
+++ src/components/acc-2025/Schedule.tsx
@@ -0,0 +1,117 @@
+import clsx from "clsx";
+
+const schedule = [
+  {
+    time: "10:00 AM",
+    title: "Welcome to Akash Accelerate",
+    speakers: ["Greg Osuri"],
+  },
+  {
+    time: "10:15 AM",
+    title: "Panel: Beyond Centralized Models",
+    speakers: [
+      "Casey Caruso, Jake Brukhman, Bidhan Roy, Benjamin Parr, Chad Fowler",
+    ],
+  },
+  {
+    time: "11:00 AM",
+    title: "Presentation: Daylight Energy",
+    speakers: ["Jason Badeaux"],
+  },
+  {
+    time: "11:30 AM",
+    title: "Fireside Chat: Energy Capacity in the Era of AI",
+    speakers: ["Greg Osuri, Javier Villamizar"],
+  },
+  {
+    time: "12:00 PM",
+    title: "Showcase: The Future of the Cloud",
+    speakers: [
+      "Arel & Lex Avellino, Zack Abrams, Daniel Keller, Nirmal Krishnan",
+    ],
+  },
+  {
+    time: "1:00 - 1:45 PM",
+    title: "Lunch Break",
+  },
+  {
+    time: "1:45 PM",
+    title: "Is DePIN Real? Building and Scaling Decentralized Networks",
+    speakers: [
+      "Tommy Eastman, Haseeb Qureshi, Mahesh Ramakrishnan, Abhay Kumar",
+    ],
+  },
+  {
+    time: "2:15 PM",
+    title: "Powering the Mind of Machines: Why America Must Decentralize AI",
+    speakers: ["Greg Osuri"],
+  },
+  {
+    time: "3:00 PM",
+    title:
+      "Giveaway Winner Announcement <br/> <span class='text-sm md:text-base'>$25,000 Akash Compute Credits</span>",
+    speakers: [],
+  },
+  {
+    time: "3:05 PM",
+    title: "Showcase: The Rise of DeAI",
+    speakers: ["Jiahao Sun, Manouk Vermaaten, Brayden Levangie"],
+  },
+  {
+    time: "3:30 PM",
+    title: "The Roadmap to a User-Owned AI Model",
+    speakers: ["Illia Polosukhin"],
+  },
+  {
+    time: "4:00 PM",
+    title: "Panel: Investing in Intelligence",
+    speakers: [
+      "Cheng Wang, Maja Vujinovic, Conor Moore, Mark Palmer, Christian Lopez",
+    ],
+  },
+  {
+    time: "5:00 PM",
+    title: "Presentation: Nous Research",
+    speakers: ["Dillon Rolnick"],
+  },
+  {
+    time: "5:30 - 7:30 PM",
+    title: "Rooftop Happy Hour",
+    speakers: ["with DJ Jon Blak"],
+  },
+];
+
+const Schedule = () => {
+  return (
+    <div className="w-full border-t">
+      {schedule.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col-reverse justify-between gap-2 border-b  py-4 md:flex-row  md:items-end"
+        >
+          <div className="flex-1">
+            <div
+              className={clsx(
+                ` font-semibold  text-lg md:text-xl`,
+                !item?.speakers && "font-instrument italic",
+              )}
+              dangerouslySetInnerHTML={{ __html: item.title }}
+            />
+            {item.speakers && item.speakers.length > 0 && (
+              <div
+                className={`mt-0.5 font-instrument text-lg font-extralight italic md:text-xl`}
+              >
+                {item.speakers}
+              </div>
+            )}
+          </div>
+          <div className=" font-jetBrainsMono text-base font-normal text-[#EAEAEA]">
+            {item.time}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Schedule;
---
+++ src/components/acc-2025/Speakers.tsx
@@ -0,0 +1,186 @@
+const speakers = [
+  {
+    name: "Greg Osuri",
+    title: "CEO Overclock Labs",
+    company: "Founder of Akash Network",
+    image: "/images/acc-2025/speakers/greg.png",
+  },
+  {
+    name: "Haseeb Qureshi",
+    title: "Founder and Managing Partner Dragonfly",
+    image: "/images/acc-2025/speakers/haseeb.png",
+  },
+  {
+    name: "Illia Polosukhin",
+    title: "Co-Founder",
+    company: "Near Protocol ",
+    image: "/images/acc-2025/speakers/Illia.png",
+  },
+  {
+    name: "Javier Villamizar",
+    title: "Operating Partner",
+    company: "SoftBank Investment Advisers",
+    image: "/images/acc-2025/speakers/javier.png",
+  },
+  {
+    name: "Jake Brukhman",
+    title: "Founder and CEO",
+    company: "CoinFund",
+    image: "/images/acc-2025/speakers/jake.png",
+  },
+  {
+    name: "Chad Fowler",
+    title: "CTO & General Partner",
+    company: "Blueyard Capital",
+    image: "/images/acc-2025/speakers/chad.png",
+  },
+  {
+    name: "Benjamin Parr",
+    title: "CoFounder",
+    company: "YesNoError, Theory Forge Ventures",
+    image: "/images/acc-2025/speakers/benjamin.png",
+  },
+  {
+    name: "Mahesh Ramakrishnan",
+    title: "Founder",
+    company: "EV3",
+    image: "/images/acc-2025/speakers/mahesh.png",
+  },
+  {
+    name: "Bidhan Roy",
+    title: "Founder and CEO",
+    company: "Bagel",
+    image: "/images/acc-2025/speakers/bidhan.png",
+  },
+  {
+    name: "Abhay Kumar",
+    title: "Head of Protocol",
+    company: "Helium",
+    image: "/images/acc-2025/speakers/abhay.png",
+  },
+  {
+    name: "Casey Caruso",
+    title: "Investor",
+    company: "Topology Ventures",
+    image: "/images/acc-2025/speakers/casey.png",
+  },
+  {
+    name: "Dillon Rolnick",
+    title: "COO",
+    company: "Nous Research ",
+    image: "/images/acc-2025/speakers/dillon.png",
+  },
+  {
+    name: "Nirmal Krishnan",
+    title: "CEO",
+    company: "Chakra Labs",
+    image: "/images/acc-2025/speakers/nirmal.png",
+  },
+
+  {
+    name: "Brayden Levangie",
+    title: "Founder and CEO",
+    company: "Levangie Laboratories",
+    image: "/images/acc-2025/speakers/Brayden.png",
+  },
+  {
+    name: "Maja Vujinovic",
+    title: "CEO",
+    company: "OGROUP",
+    image: "/images/acc-2025/speakers/maja.png",
+  },
+  {
+    name: "Mark Palmer",
+    title: "Managing Director/Senior ",
+    company: "Equity Research Analyst <br/> The Benchmark Company",
+    image: "/images/acc-2025/speakers/mark.png",
+  },
+  {
+    name: "Manouk Termaaten",
+    title: "Founder and CEO",
+    company: "Vertical Studio BV",
+    image: "/images/acc-2025/speakers/manouk.png",
+  },
+  {
+    name: "Jiahao Sun",
+    title: "CEO",
+    company: "FLock.io",
+    image: "/images/acc-2025/speakers/jiahao.png",
+  },
+  {
+    name: "Jason Badeaux",
+    title: "Cofounder and CEO",
+    company: "Daylight",
+    image: "/images/acc-2025/speakers/Jason.png",
+  },
+  {
+    name: "Tommy Eastman",
+    title: "Research Lead",
+    company: "Plaintext Capital",
+    image: "/images/acc-2025/speakers/tommy.png",
+  },
+  {
+    name: "Zack Abrams",
+    title: "AI Video Creator",
+    company: "",
+    image: "/images/acc-2025/speakers/zack.png",
+  },
+  {
+    name: "Arel Avellino",
+    title: "CEO",
+    company: "Passage",
+    image: "/images/acc-2025/speakers/arel.png",
+  },
+  {
+    name: "Lex Avellino",
+    title: "Founder and CCO",
+    company: "Passage",
+    image: "/images/acc-2025/speakers/lex.png",
+  },
+  {
+    name: "Daniel Keller",
+    title: "Founder and CEO",
+    company: "InFlux Technologies",
+    image: "/images/acc-2025/speakers/daniel.png",
+  },
+  {
+    name: "Conor Moore",
+    title: "Co-Founder & COO",
+    company: "Permian Labs",
+    image: "/images/acc-2025/speakers/conor.png",
+  },
+];
+
+const SpeakersContent = () => {
+  return (
+    <div className="grid grid-cols-2 gap-x-3 gap-y-5 md:gap-x-5 md:gap-y-12 lg:grid-cols-4">
+      {speakers.map((speaker) => (
+        <div className="flex flex-col gap-2 md:gap-3">
+          <img
+            src={speaker.image}
+            alt={speaker.name}
+            className=" aspect-[16/13.3] w-full bg-[#363636] object-cover"
+          />
+          <div className="flex flex-col gap-3">
+            <p className="text-sm font-medium !leading-[1.2] text-foreground md:text-xl">
+              {speaker.name}
+            </p>
+            <div className="flex flex-col ">
+              <p className="text-xs font-medium !leading-[1.2] text-[#909090]  md:text-lg">
+                {speaker.title}
+              </p>
+              {speaker.company && (
+                <p
+                  dangerouslySetInnerHTML={{ __html: speaker.company }}
+                  className="text-xs font-medium !leading-[1.2] text-[#909090] md:text-lg"
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SpeakersContent;
