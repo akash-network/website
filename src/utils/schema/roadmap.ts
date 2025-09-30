d604316a531cd48db8baf581b786452e1f1e56b5
Dharamveer singh <dharamveersingh.em@gmail.com>
2025-09-30 10:28:32 +0530

    chore: remove Ray Summit event content and associated banner image

--- src/utils/schema/roadmap.ts
+++ src/utils/schema/roadmap.ts
@@ -2,13 +2,13 @@ import { defineCollection, z } from "astro:content";
 
 export const roadmapSchema = defineCollection({
   schema: z.object({
-    aep: z.number(),
-    title: z.string(),
-    author: z.string(),
-    status: z.string(),
-    type: z.string(),
+    aep: z.number().optional(),
+    title: z.string().optional(),
+    author: z.string().optional(),
+    status: z.string().optional(),
+    type: z.string().optional(),
     category: z.string().optional(),
-    created: z.coerce.date(),
+    created: z.coerce.date().optional(),
     updated: z.coerce.date().optional(),
     "estimated-completion": z.coerce.date().optional(),
     roadmap: z.enum(["major", "minor"]).optional(),
